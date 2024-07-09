9de0484be29000c2f6ae2c73bfac1ded45fe0744
KyanChin <158687185+KyanChin@users.noreply.github.com>
2024-07-09 17:01:56 +0800

    Create basics.js

---
+++ basics.js
@@ -0,0 +1,40 @@
+let myname = 'Kyan';
+const myage = 18;
+let myphone = 12345678;
+
+let myarr = [1, 2, 3, 4, 5];
+let ishuman = true; 
+
+console.log(myname);
+
+let a = (5 + 7) * 2;
+let b = (5 - 7) / 3;
+let c = 5 ** 7;
+let d = 123 % 45;
+
+console.log(a, b, c, d);
+
+let e = Math.random();
+let f = Math.round(123.45678);
+let g = Math.cos(Math.PI);
+let h = Math.E ** 2;
+
+console.log('cos(pi) = ' + g);
+
+
+let firstname = 'Andrew';
+let lastname = 'Tay';
+let fullname = firstname + ' ' + lastname;
+let namelength = fullname.length;
+
+console.log(fullname + ', length: ' + namelength);
+
+let language = 'Javascript';
+console.log(language[0]);
+console.log(language.substring(2, 5));
+console.log(language.toUpperCase());
+console.log(language.replace('Java', 'Type'));
+console.log(language.includes('Java'));
+
+console.log(`But I also like ${language.toLowerCase()}!`)
+
